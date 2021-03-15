03dbb6b7aaa593ccd828664041602955bc0255a6
sammy <sammyoina@gmail.com>
2021-03-15 12:14:40 +0300

    write to file

--- spikeless_measure.py
+++ spikeless_measure.py
@@ -4,6 +4,9 @@ import RPi.GPIO as GPIO
 from scale import Scale
 import time
 from statistics import mean
+from datetime import datetime
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def Calibration(scale):
     readings = []
@@ -21,6 +24,7 @@ def Calibration(scale):
 
     return mean(readings)/refWeight
 
+filename = datetime.now().strftime("%A %d %B %Y %I-%M%p") + ".csv"
 
 scale = Scale()
 
@@ -30,13 +34,23 @@ scale.reset()
 scale.tare()
 
 scale.setReferenceUnit(Calibration(scale))
-
+presses = 0
 while True:
 
     try:
         val = scale.getMeasure()
+        if (GPIO.input(18) == False):
+            presses = presses + 1
         print("{0: 4.4f}".format(val))
+        if(presses == 1):
+            f = open(filename, "a")
+            print("File Created")
+        elif (presses == 2):
+            f.write(val + "\n")
+        elif (presses > 2):
+            f.close()
 
     except (KeyboardInterrupt, SystemExit):
+        f.close()
         GPIO.cleanup()
         sys.exit()
