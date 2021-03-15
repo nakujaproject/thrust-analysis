251a8e003699959ef43fc67583a38924a1b23e8e
sammy <sammyoina@gmail.com>
2021-03-15 12:40:57 +0300

    write to file

--- measure.py
+++ measure.py
@@ -1,9 +1,13 @@
 import sys
 import time
 from statistics import mean
+from datetime import datetime
 
 import RPi.GPIO as GPIO
 from hx711 import HX711
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def Calibration(hx):
     readings = []
@@ -21,6 +25,10 @@ def Calibration(hx):
 
     return mean(readings)/refWeight
 
+filename = datetime.now().strftime("%A %d %B %Y %I-%M%p") + ".csv"
+f = open(filename, "a")
+print("File Created")
+
 # choose pins on rpi (BCM5 and BCM6)
 hx = HX711(dout=5, pd_sck=6)
 
@@ -30,12 +38,21 @@ hx.reset()
 hx.tare()
 
 hx.setReferenceUnit(Calibration(hx))
+
+presses = 0
 while True:
 
     try:
         val = hx.getWeight()
+        if (GPIO.input(18) == False):
+            presses = presses + 1
         print("{0: 4.4f}".format(val))
+        if (presses == 1):
+            f.write(str(val) + "\n")
+        elif (presses > 1):
+            f.close()
 
     except (KeyboardInterrupt, SystemExit):
+        f.close()
         GPIO.cleanup()
         sys.exit()
